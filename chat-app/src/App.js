83836f526544ff27511712776a643cb194db6856
bruno.fernandes <brunofernandes@softbox.com.br>
2019-09-08 21:55:04 -0300

    constructor

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Router } from 'react-router-dom'
 import { StylesProvider } from '@material-ui/styles'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { CssBaseline } from '@material-ui/core'
 import { history } from 'commons/utils/router'
-import theme from 'constants/theme'
+import theme from 'commons/constants/theme'
 import GlobalStyle from 'commons/styles/global-style'
 import Main from './containers/Main'
 
@@ -14,11 +14,13 @@ const App = () => (
   <StylesProvider injectFirst>
     <MuiThemeProvider theme={theme}>
       <StyledThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyle />
+        <Fragment>
+          <CssBaseline />
+          <GlobalStyle />
           <Router history={history}>
             <Main />
           </Router>
+        </Fragment>
       </StyledThemeProvider>
     </MuiThemeProvider>
   </StylesProvider>
--- chat-app/src/containers/Main/index.jsx
+++ chat-app/src/containers/Main/index.jsx
@@ -1,14 +1,16 @@
-import React, { memo } from 'react'
+import React, { Suspense, memo } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import routes from '../../routes'
 
 const PageNotFound = () => <Redirect to="/" />
 const Main = () => (
   <div>
-    <Switch>
-      {routes.map(route => <Route key={route.path} {...route} />)}
-      <Route component={PageNotFound} />
-    </Switch>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Switch>
+        {routes.map(route => <Route key={route.path} {...route} />)}
+        <Route component={PageNotFound} />
+      </Switch>
+    </Suspense>
   </div>
 )
 export default memo(Main)
